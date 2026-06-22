48bf216f381b20fb99abacf3cf080dff60501c88
sims-2hub <badamiants@sims2.ru>
2026-06-22 21:39:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-const variants = [
-{
-name:“Голос SIMS-2”,
-description:“Площадка для вопросов, предложений и обратной связи сотрудников. Здесь можно поднять важную тему для обсуждения на полугодовой встрече с руководством.”
-},
-{
-name:“Открытый диалог”,
-description:“Пространство для открытого общения сотрудников и руководства.”
-},
-{
-name:“SIMS-2 слушает”,
-description:“Канал обратной связи для вопросов, идей и предложений сотрудников.”
-},
-{
-name:“Ваш вопрос — в повестку”,
-description:“Помогите сформировать темы для обсуждения на полугодовой встрече.”
-},
-{
-name:“Диалог с руководством”,
-description:“Площадка для вопросов и предложений сотрудников.”
-}
-];
+document.addEventListener("DOMContentLoaded", function () {
 
-function changeVariant(index){
+    console.log("Голос SIMS-2 запущен");
 
-document.getElementById(“projectName”).innerText =
-variants[index].name;
-
-document.getElementById(“projectDescription”).innerText =
-variants[index].description;
-
-document.title = variants[index].name;
-
-}
+});
